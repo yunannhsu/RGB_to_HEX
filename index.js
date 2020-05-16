98b2ef047cb8b437941784088e47930f49755675
yunannhsu <62492094+yunannhsu@users.noreply.github.com>
2020-05-16 14:07:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,52 +3,48 @@ const displayHexNum = document.querySelector(".display-hex-num")
 const displayHexColor = document.querySelector(".display-hex-color")
 
 convertBtn.addEventListener('click',function(event){
-  const inputvalue1= document.querySelector("#r-input").value
-  const inputvalue2= document.querySelector("#g-input").value
-  const inputvalue3= document.querySelector("#b-input").value
-  const iv1 = Number(inputvalue1)
-  const iv2 = Number(inputvalue2)
-  const iv3 = Number(inputvalue3)
+  let inputvalue1= Number(document.querySelector("#r-input").value)
+  let inputvalue2= Number(document.querySelector("#g-input").value)
+  let inputvalue3= Number(document.querySelector("#b-input").value)
   
-  if((inputvalue1.length == 0 )|(inputvalue2.value == 0  )|(inputvalue3.value == 0 )){
+  if((inputvalue1|inputvalue2|inputvalue3) > 255 |(inputvalue1|inputvalue2|inputvalue3) <0 ){
+    document.querySelector("#r-input").value = null
+    document.querySelector("#g-input").value = null
+    document.querySelector("#b-input").value = null
+    alert("輸入欄範圍必須在數字 0~225 之間")
+  }
+  else{
+    hexNum = switchToHex(inputvalue1,inputvalue2,inputvalue3).toUpperCase()
   console.log(inputvalue1,inputvalue2,inputvalue3)
-  displayHexNum.innerText = ''
-  displayHexColor.style.backgroundColor = ''
-  console.log("noprintblank")}
-  
-  else if ((iv1 >= 0 & iv1 <= 255)&(iv2 >= 0 & iv2 <= 255 )&(iv3 >= 0 & iv3 <= 255 )){
-  hexNum= switchToHex(iv1,iv2,iv3).toUpperCase()
-  console.log(iv1,iv2,iv3)
   displayHexNum.innerText = hexNum
   displayHexColor.style.backgroundColor = '#'+ hexNum
-  console.log("print")    }
-  
-  else{
-  console.log(inputvalue1,inputvalue2,inputvalue3)
-  displayHexNum.innerText = ''
-  displayHexColor.style.backgroundColor = ''
-  console.log("noprint")
-      }    })      
+  console.log("print") 
+  }})
+                
 
-function bgcolor() {
-  return displayHexColor.innerHTML
+function bgcolor(){
+  return  displayHexColor.innerHTML
 }
 
-function switchToHex(rdec, gdec, bdec) {
-  const rHex = rdec.toString(16)
-  const gHex = gdec.toString(16)
-  const bHex = bdec.toString(16)
-  return (pad(rHex) + pad(gHex) + pad(bHex))
-}
-function pad(str) {
+function switchToHex(rdec,gdec,bdec){
+   const rHex = rdec.toString(16)
+   const gHex = gdec.toString(16)
+   const bHex = bdec.toString(16)
+   return(pad(rHex)+pad(gHex)+pad(bHex))
+    }
+function pad(str){
   str = '' + str
-  if (str.length < 2) {
-    str = "0" + str
-    //console.log (str)
-    return str
-  } else {
-    //console.log (str)
-    return str;
-  }
-}
+  if (str.length<2){
+    str = "0"+str
+      //console.log (str)
+      return str
+    } else {
+      //console.log (str)
+      return str;
+       }}
+
+
+
+
+
 
